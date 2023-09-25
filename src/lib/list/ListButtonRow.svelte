c5174f1e2807abef1836b0c059bd04ca27084536
Carza-104 <carzaniga-github@proton.me>
2023-09-25 14:38:53 +0200

    Create ListButtonRow and ListTextFieldRow

---
+++ src/lib/list/ListButtonRow.svelte
@@ -0,0 +1,48 @@
+<!-- @format -->
+<script>
+    export let type = "enabled";
+    /* export let type = "destructive"; */
+    /* export let type = "disabled"; */
+    export let label = "Title";
+
+    export let id = undefined;
+    export let onPress = undefined;
+    export let style = undefined;
+
+    export let colorStyle = "unset";
+    if (type === "enabled") {
+        colorStyle = "var(--colors-accent)";
+    } else if (type === "destructive") {
+        colorStyle = "var(--colors-red)";
+    } else {
+        colorStyle = "var(--grays-gray-2)";
+    }
+
+    if (type === "disabled") {
+        onPress = undefined;
+    }
+</script>
+
+<label {id} {style}>
+    <button class="body" on:click={onPress} style="color: {colorStyle}"
+        >{label}</button
+    >
+</label>
+
+<style>
+    label {
+        align-items: center;
+        background: var(--bg-grouped-secondary);
+        display: flex;
+        height: 44px;
+        padding-left: 16px;
+    }
+
+    button {
+        align-items: center;
+        border-bottom: 0.33px solid var(--separators-non-opaque);
+        display: flex;
+        flex: 1;
+        height: 100%;
+    }
+</style>
---
+++ src/lib/list/ListTextFieldRow.svelte
@@ -0,0 +1,47 @@
+<!-- @format -->
+<script>
+    export let title = "Title";
+    export let placeholder = "Value";
+
+    export let id = undefined;
+    export let onPress = undefined;
+    export let style = undefined;
+</script>
+
+<label {style}>
+    <div>
+        <p class="body">{title}</p>
+        <input class="body" {id} on:click={onPress} {placeholder} type="text" />
+    </div>
+</label>
+
+<style>
+    label {
+        align-items: center;
+        background: var(--bg-grouped-secondary);
+        display: flex;
+        height: 44px;
+        padding-left: 16px;
+    }
+
+    div {
+        align-items: center;
+        border-bottom: 0.33px solid var(--separators-non-opaque);
+        display: flex;
+        flex: 1;
+        height: 100%;
+        padding-right: 16px;
+    }
+
+    p {
+        width: 100px;
+    }
+
+    input {
+        width: 100%;
+    }
+
+    input::placeholder {
+        color: var(--labels-secondary);
+    }
+</style>
