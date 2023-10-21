22d7ae54a65494987ac7fc2b7390c1eff2b881ba
Carza-104 <carzaniga-github@proton.me>
2023-10-21 20:07:21 +0200

    Initial commit

---
+++ src/lib/page-control/PageControl.svelte
@@ -0,0 +1,42 @@
+<script>
+	export let showPlatter = false;
+
+	export let id = undefined;
+	export let style = undefined;
+
+	let backdropFilterStyle = 'unset';
+	let backgroundStyle = 'unset';
+
+	if (showPlatter) {
+		backdropFilterStyle = '20px';
+		backgroundStyle = 'var(--materials-ultrathin)';
+	}
+</script>
+
+<div class="page-control" {id} {style}>
+	<div
+		class="platter"
+		style="backdrop-filter: {backdropFilterStyle}; background: {backgroundStyle}; -webkit-backdrop-filter {backdropFilterStyle}"
+	>
+		<slot />
+	</div>
+</div>
+
+<style>
+	.page-control {
+		align-items: center;
+		display: flex;
+		height: 44px;
+		justify-content: center;
+	}
+
+	.platter {
+		align-items: center;
+		border-radius: 12px;
+		display: flex;
+		gap: 8px;
+		justify-content: center;
+		padding: 8px 12px;
+		width: min-content;
+	}
+</style>
---
+++ src/lib/page-control/PageControlButton.svelte
@@ -0,0 +1,55 @@
+<script>
+	export let inputGroup = 'page-control-button';
+	export let state = 'default';
+	/* export let state = 'selected'; */
+
+	export let id = undefined;
+	export let onPress = undefined;
+	export let style = undefined;
+
+	let inputElement = undefined;
+
+	function handlePress() {
+		if (onPress) {
+			onPress();
+		}
+		inputElement.blur();
+	}
+</script>
+
+<label {style}>
+	{#if state === 'selected'}
+		<input
+			bind:this={inputElement}
+			checked
+			class="hidden-input"
+			{id}
+			name={inputGroup}
+			on:click={handlePress}
+			type="radio"
+		/>
+	{:else}
+		<input
+			bind:this={inputElement}
+			class="hidden-input"
+			{id}
+			name={inputGroup}
+			on:click={handlePress}
+			type="radio"
+		/>
+	{/if}
+</label>
+
+<style>
+	label {
+		background: var(--labels-primary);
+		border-radius: 50%;
+		height: 8px;
+		opacity: 30%;
+		width: 8px;
+	}
+
+	label:has(> input:checked) {
+		opacity: unset;
+	}
+</style>
