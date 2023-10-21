2a92f4b77d0e1be1732aa60b5154264e9ca900ba
Carza-104 <carzaniga-github@proton.me>
2023-10-21 14:24:55 +0200

    Initial commit

---
+++ src/lib/list/ListSliderRow.svelte
@@ -0,0 +1,99 @@
+<script>
+	export let value = undefined;
+	export let min = '0';
+	export let max = '100';
+	export let showSymbols = false;
+	export let minSymbol = 'wb_sunny';
+	export let maxSymbol = 'sunny';
+
+	export let id = undefined;
+	export let style = undefined;
+</script>
+
+<label {style}>
+	<div>
+		{#if showSymbols}
+			<p class="symbol">{minSymbol}</p>
+		{/if}
+		<input bind:value class="body" {id} {max} {min} type="range" />
+		{#if showSymbols}
+			<p class="symbol">{maxSymbol}</p>
+		{/if}
+	</div>
+</label>
+
+<style>
+	label {
+		align-items: center;
+		background: var(--bg-grouped-secondary);
+		display: flex;
+		height: 44px;
+		padding-left: 16px;
+	}
+
+	div {
+		align-items: center;
+		border-bottom: 0.33px solid var(--separators-non-opaque);
+		display: flex;
+		flex: 1;
+		flex-direction: row;
+		gap: 7px;
+		height: 100%;
+		padding-right: 16px;
+	}
+
+	.symbol {
+		align-items: center;
+		color: var(--labels-secondary);
+		display: flex;
+		font-size: calc(17px * var(--medium-symbol-font-size-multiplier));
+		font-weight: calc(590 / var(--medium-symbol-font-size-multiplier));
+		height: 22px;
+		justify-content: center;
+		line-height: 22px;
+		width: 22px;
+	}
+
+	input[type='range'] {
+		appearance: none;
+		align-items: center;
+		border-radius: 2px;
+		background: var(--fills-secondary);
+		cursor: pointer;
+		display: flex;
+		height: 4px;
+		width: 100%;
+	}
+
+	input[type='range']::-moz-range-thumb {
+		appearance: none;
+		background: var(--grays-white);
+		border: none;
+		border-radius: 50%;
+		box-shadow: 0px 6px 13px rgb(0, 0, 0, 0.12), 0px 0.5px 4px rgb(0, 0, 0, 0.12);
+		height: 28px;
+		width: 28px;
+	}
+
+	input[type='range']::-webkit-slider-thumb {
+		appearance: none;
+		background: var(--grays-white);
+		border-radius: 50%;
+		box-shadow: 0px 6px 13px rgb(0, 0, 0, 0.12), 0px 0.5px 4px rgb(0, 0, 0, 0.12);
+		height: 28px;
+		width: 28px;
+	}
+
+	input[type='range']::-moz-range-progress {
+		appearance: none;
+		border-radius: 2px;
+		background: var(--colors-accent);
+		cursor: pointer;
+		height: 4px;
+		width: 100%;
+	}
+
+	label:last-child > div {
+		border-bottom: unset;
+	}
+</style>
