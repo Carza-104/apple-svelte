760dcb239e4fa2a859a99f17f1c5966916b2b72c
Carza-104 <carzaniga-github@proton.me>
2023-10-21 19:07:01 +0200

    Update background

--- src/lib/alert/AlertTextField.svelte
+++ src/lib/alert/AlertTextField.svelte
@@ -10,9 +10,22 @@
 <input bind:value class="body" {id} on:click={onPress} {placeholder} {style} type="text" />
 
 <style>
+	:root {
+		color-scheme: light dark;
+		--background: var(--grays-white);
+		--border: rgb(60, 60, 67, 0.29);
+	}
+
+	@media (prefers-color-scheme: dark) {
+		:root {
+			--background: var(--bg-grouped-secondary);
+			--border: rgb(84, 84, 88, 0.65);
+		}
+	}
+
 	input {
-		background: var(--grays-white);
-		border: 0.5px solid rgb(60, 60, 67, 0.29);
+		background: var(--background);
+		border: 0.5px solid var(--border);
 		border-radius: 5px;
 		margin-top: 14px;
 		padding: 2px 6px;
