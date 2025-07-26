4dd6c2621655e3b10597db07ecbdc56f701a9443
Carza-104 <carzaniga-github@proton.me>
2025-07-26 12:15:50 +0200

    Update mix-blend-mode

--- src/lib/liquid-glass-alert/LiquidGlassAlertTextField.svelte
+++ src/lib/liquid-glass-alert/LiquidGlassAlertTextField.svelte
@@ -27,7 +27,7 @@
 		background: var(--fills-secondary);
 		border-radius: 26px;
 		margin-bottom: 19px;
-		mix-blend-mode: plus-darker;
+		mix-blend-mode: var(--liquid-glass-secondary-tertiary-blend-mode);
 		padding: 16px;
 		width: calc(100% - 28px);
 	}
