d9896ffe33086c3c9fca25c229e508ddf6e2af3b
Carza-104 <carzaniga-github@proton.me>
2025-08-01 18:13:29 +0200

    Initial commit

---
+++ src/lib/ios-26-tab-bar/iOS26TabBar.svelte
@@ -0,0 +1,35 @@
+<script>
+	export let id = undefined;
+	export let style = undefined;
+</script>
+
+<div class="liquid-glass liquid-glass-medium" {id} {style}>
+	<slot />
+</div>
+
+<style>
+	div {
+		border-radius: 66px;
+		bottom: 0px;
+		box-shadow: 0px -0.33px rgb(0, 0, 0, 0.3);
+		display: flex;
+		justify-content: space-between;
+		margin: 16px 28px 32px;
+		padding: 4px;
+		position: fixed;
+	}
+
+	div::before {
+		border-radius: 66px;
+	}
+
+	div::after {
+		border-radius: 66px;
+	}
+
+	@media (prefers-color-scheme: dark) {
+		div {
+			box-shadow: 0px -0.33px rgb(255, 255, 255, 0.15);
+		}
+	}
+</style>
