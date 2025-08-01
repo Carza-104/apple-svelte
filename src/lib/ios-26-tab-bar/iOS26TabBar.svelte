ad53e1b8b1d5994d0e94aea03aca26558e30fc6c
Carza-104 <carzaniga-github@proton.me>
2025-08-01 18:17:10 +0200

    Remove unnecessary box-shadow

--- src/lib/ios-26-tab-bar/iOS26TabBar.svelte
+++ src/lib/ios-26-tab-bar/iOS26TabBar.svelte
@@ -11,7 +11,6 @@
 	div {
 		border-radius: 66px;
 		bottom: 0px;
-		box-shadow: 0px -0.33px rgb(0, 0, 0, 0.3);
 		display: flex;
 		justify-content: space-between;
 		margin: 16px 28px 32px;
@@ -25,11 +24,5 @@
 
 	div::after {
 		border-radius: 66px;
-	}
-
-	@media (prefers-color-scheme: dark) {
-		div {
-			box-shadow: 0px -0.33px rgb(255, 255, 255, 0.15);
-		}
 	}
 </style>
